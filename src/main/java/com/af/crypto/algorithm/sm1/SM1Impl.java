1843d0a952f8d345cf24ce7f216ee408130a58d3
zzypersonally@gmail.com <zzypersonally@gmail.com>
2023-05-05 15:37:33 +0800

    结构体构建

--- src/main/java/com/af/crypto/algorithm/sm1/SM1Impl.java
+++ src/main/java/com/af/crypto/algorithm/sm1/SM1Impl.java
@@ -1,5 +1,8 @@
 package com.af.crypto.algorithm.sm1;
 
+import com.af.constant.GroupMode;
+import com.af.exception.AFCryptoException;
+
 /**
  * @author zhangzhongyuan@szanfu.cn
  * @description
@@ -8,4 +11,35 @@ package com.af.crypto.algorithm.sm1;
 public class SM1Impl implements SM1 {
 
 
+    /**
+     * SM1加密
+     *
+     * @param mode  分组模式 ECB/CBC
+     * @param index 内部密钥索引 如果使用外部密钥，此参数无效
+     * @param key   外部密钥 如果使用内部密钥，此参数无效
+     * @param data  待加密数据
+     * @param IV    初始向量 如果使用ECB模式，此参数无效
+     * @return 加密后的数据
+     * @throws AFCryptoException 加密异常
+     */
+    @Override
+    public byte[] SM1Encrypt(GroupMode mode, int index, byte[] key, byte[] data, byte[] IV) throws AFCryptoException {
+        return new byte[0];
+    }
+
+    /**
+     * SM1解密
+     *
+     * @param mode  分组模式 ECB/CBC
+     * @param index 内部密钥索引 如果使用外部密钥，此参数无效
+     * @param key   外部密钥 如果使用内部密钥，此参数无效
+     * @param data  待解密数据
+     * @param IV    初始向量 如果使用ECB模式，此参数无效
+     * @return 解密后的数据
+     * @throws AFCryptoException 解密异常
+     */
+    @Override
+    public byte[] SM1Decrypt(GroupMode mode, int index, byte[] key, byte[] data, byte[] IV) throws AFCryptoException {
+        return new byte[0];
+    }
 }
